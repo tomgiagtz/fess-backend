78ebd5f0bb46b427f497bd8520c70573fc22bd98
tomgiagtz <tom.giagtzoglou@gmail.com>
2018-11-12 13:21:04 -0500

    create path for posts done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,22 @@ class PostsController < ApplicationController
   def index
   	render json: Post.all
   end
+
+  def create
+  	puts "params" + post_params.to_s
+  	post = Post.new
+  	post.content = post_params[:content]
+  	post.location = ActiveRecord::Point.new(post_params[:location][:x], post_params[:location][:y])
+  	post.save
+  
+  	render json: post
+  end	
+
+
+
+  private
+
+  def post_params
+  	params.require(:post).permit(:content, location: [:x, :y])
+  end
 end
