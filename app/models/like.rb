6458668c0eb8bb8f3816dca1b426b6c12561df4e
tomgiagtz <tom.giagtzoglou@gmail.com>
2018-11-14 14:00:35 -0500

    removed like validations

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,10 +1,4 @@
 class Like < ApplicationRecord
   belongs_to :post
   belongs_to :user
-  validates :user_id, numericality: { only_integer: true}
-  validates :post_id, numericality: { only_integer: true}
-  validates :user_id, presence: true
-  validates :post_id, presence: true
-  validates :upvote, presence: true
-  validates :upvote, inclusion: {in: [true, false]}
 end
